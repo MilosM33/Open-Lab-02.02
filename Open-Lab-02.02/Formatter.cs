6e1290299f8781d831497d0e4135a72b08af0520
MilosM33 <milosmarejka89@gmail.com>
2020-09-25 15:38:24 +0200

    String format

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_02._02
 {
     public class Formatter
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+           
+            string text = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                text += Math.Round(numbers[i], 2) + (i +1==numbers.Length ? "" : " ");
+            }
+
+            Console.Write(text.Replace(',','.'));
+            
         }
     }
 }
